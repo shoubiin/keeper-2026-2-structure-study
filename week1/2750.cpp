2f4f86fa2e1cdefd417b7b99cb7b77433ba8f342
shoubiin <subins2568@gmail.com>
2026-09-21 17:15:21 +0900

    백준2750 풀이

--- week1/2750.cpp
+++ week1/2750.cpp
@@ -15,7 +15,7 @@ int main() {
         cin >> a[i];
     }
 
-    sort(a.begin(), a.end());
+    sort(a.begin(), a.end()); //sort 함수를 기억하자..
 
     for (int i = 0; i < n; i++) {
         cout << a[i] << '\n';
